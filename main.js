90ec3fcb97da989a51597512b9695cff36311d42
Falutin Raven <falutinraven@gmail.com>
2024-12-17 15:04:04 -0600

    change to classes

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const myLibrary = [];
 
-function Book(name, author, pages, read) {
-  this.name = name;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(name, author, pages, read){
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function addBookToLibrary(library, name, author, pages, read) {
